95b533c84494b482c42c729b0f16af1f2603b9e8
Isuru <isuru@venturit.com>
2017-04-19 12:02:44 +0530

    Fixing deployment issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+$:.push File.expand_path("vendor/campusties_engine/lib", __FILE__)
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
